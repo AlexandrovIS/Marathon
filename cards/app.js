a98878061549f60f0b2e0f64c3d242a6dbebec95
alexandrovis <Alexandrovis@gmail.com>
2021-12-16 17:07:14 +0300

    cards with cars

---
+++ cards/app.js
@@ -0,0 +1,11 @@
+const slides=document.querySelectorAll('.slide')
+
+slides.forEach(item=>item.addEventListener('click',()=>{
+    clearActiveClasses()
+    item.classList.add('active')
+  })
+)
+
+function clearActiveClasses(){
+  slides.forEach(item=>item.classList.remove('active'))
+}
