bcfb43d09d1c5f7354d2b08f7e096e61d03df578
alexandrovis <Alexandrovis@gmail.com>
2021-12-17 13:27:07 +0300

    correct

--- cards/app.js
+++ cards/app.js
@@ -1,11 +1,16 @@
-const slides=document.querySelectorAll('.slide')
+function chooseImage(activeImage=0){
+  const slides=document.querySelectorAll('.slide')
+  slides[activeImage].classList.add('active')
 
-slides.forEach(item=>item.addEventListener('click',()=>{
-    clearActiveClasses()
-    item.classList.add('active')
-  })
-)
+  slides.forEach(item=>item.addEventListener('click',()=>{
+      clearActiveClasses()
+      item.classList.add('active')
+    })
+  )
 
-function clearActiveClasses(){
-  slides.forEach(item=>item.classList.remove('active'))
+  function clearActiveClasses(){
+    slides.forEach(item=>item.classList.remove('active'))
+  }
 }
+
+chooseImage(1)
