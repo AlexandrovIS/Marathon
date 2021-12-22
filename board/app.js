9aefe9d111e4c9d22c9957ae6bf8f7c3edef8089
alexandrovis <Alexandrovis@gmail.com>
2021-12-22 16:21:45 +0300

    другой вариант

--- board/app.js
+++ board/app.js
@@ -1,29 +1,42 @@
 const board=document.querySelector('#board')
-const colors=['red','blue','green','yellow','pink','purple']
-const SQUARES_NUMBER=500
+const listColor=document.querySelector('.list-color')
+const colors=['red','blue','green','yellow','pink','white','purple']
+const SQUARES_NUMBER=900
+let currentColor='#fff'
 
 for(let i=0;i<SQUARES_NUMBER;i++){
   const square=document.createElement('div')
   square.classList.add('square')
 
-  square.addEventListener('mouseover',()=>setColor(square))
-  square.addEventListener('mouseleave',()=>removeColor(square))
-
+  square.addEventListener('mousemove',()=>setColor(square,currentColor))
+  
   board.append(square)
 }
 
-function setColor(el){
-  const color=getRandomColor()
+colors.forEach(color=>{
+  const itemColor=document.createElement('div')
+  itemColor.classList.add('item-color')
+  itemColor.style.backgroundColor=`${color}`
+  listColor.append(itemColor)
+
+  itemColor.addEventListener('click',e=>currentColor=e.target.style.backgroundColor)
+})
+
+function setColor(el,color){
   el.style.backgroundColor=`${color}`
-  el.style.boxShadow=`0 0 2px ${color}, 0 0 10px ${color}`
+  if(color==='#fff'|| color==='rgb(255, 255, 255)'||color==='white'){
+    el.style.boxShadow=`0 0 2px #1d1d1d, 0 0 10px ${color}`
+  } else {
+    el.style.boxShadow=`0 0 2px ${color}, 0 0 10px ${color}`
+  }
+  
 }
 
-function removeColor(el){
-  el.style.backgroundColor=`#1d1d1d`
-  el.style.boxShadow=`0 0 2px #111`
+function print(e){
+  console.log(e)
 }
+// function removeColor(el){
+//   el.style.backgroundColor=`#fff`
+//   el.style.boxShadow=`0 0 2px #1d1d1d`
+// }
 
-function getRandomColor(){
-  const index=Math.floor(Math.random()*colors.length)
-  return colors[index]
-}
